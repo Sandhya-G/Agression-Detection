7d7d329e66ff1c931071e9d6d70788b4bf396a28
Sandhya G <sandhyag699@gmail.com>
2020-03-11 20:18:47 +0530

    fixed bug in spellings

--- src/processing/spellings.py
+++ src/processing/spellings.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import re
 from spellchecker import SpellChecker
 
 #assumes each character doesn't occur more than 2 times at once (really simple logic)
@@ -7,17 +7,24 @@ import itertools
 def replace_elongated_text(text):
     text = ''.join(''.join(s)[:2] for _, s in itertools.groupby(text))
     return text
-print(replace_elongated_text("aaaaaabbbbbbaaaaccccddc haaaappppyyyyy"))
+#print(replace_elongated_text("aaaaaabbbbbbaaaaccccddc 3333222211 haaaappppyyyyy"))
 
-## uses a Levenshtein Distance algorithm to find permutations within an edit distance(dp problem) of 2 from the original word.
+def words(text): 
+    return re.findall('[a-z]+', text.lower()) 
 
+# uses a Levenshtein Distance algorithm to find permutations within an edit distance(dp problem) of 2 from the original word.
 def spell_checker(text):
     spell = SpellChecker()
+    only_text = " ".join(words(text))
+    print(only_text)
     # find those words that may be misspelled
-    list_of_words = text.split()
+    list_of_words = only_text.strip().split()
     misspelled = spell.unknown(list_of_words)
-    new_text = ''
-    for word in list_of_words:
-        new_text = new_text + " " +spell.correction(word) if word in misspelled else word
-    return new_text
-print(spell_checker("haappyy birtday"))
+    #print("mis",misspelled)
+    text=text.lower()
+    for word in misspelled:
+        text = text.replace(word,spell.correction(word))
+    return text
+#print(spell_checker("3Blue1Vrown"))
+#print("10 is a number")
+#print(spell_checker("haappyy birtday"))
