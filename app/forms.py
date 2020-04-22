c3cd5a7aa687b5314334d20bd47b5423a02c24f0
Sandhya G <sandhyag699@gmail.com>
2020-04-22 23:01:39 +0530

    login forms

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class SignUp(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('SignIn')
--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,29 @@ from app import app
 import pickle
 import numpy as np
 from pathlib import Path
-from flask import render_template
+from flask import render_template,flash,redirect,url_for
+from app.forms import LoginForm,SignUp
+
 
 @app.route('/')
-@app.route('/home')
+@app.route('/index')
 def index():
 	# Pkl_Filename = Path("model/Pickle_KNN_Model.pkl")
 	# with open(Pkl_Filename, 'rb') as file:  
 	# 	Pickled_KNN_Model = pickle.load(file)
 	# return str(Pickled_KNN_Model)
-	 
+
 	return render_template('home.html')
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}'.format(form.username.data),category='success')
+		return redirect(url_for('index'))
+	return render_template('login.html',form=form)
+
+@app.route('/signup')
+def signup():
+	form = SignUp()
+	return render_template('signup.html',form=form) 
