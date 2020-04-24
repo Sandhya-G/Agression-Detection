edb4bbbdec63d42ea6873e8783d6468a386c2e0f
Sandhya G <sandhyag699@gmail.com>
2020-04-25 02:58:51 +0530

    frontend completed

--- app/routes.py
+++ app/routes.py
@@ -5,22 +5,38 @@ from pathlib import Path
 from flask import render_template,flash,redirect,url_for,request
 from app.forms import LoginForm,SignUp
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from app import db
 from werkzeug.urls import url_parse
 
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
 @login_required
 def index():
-	# Pkl_Filename = Path("model/Pickle_KNN_Model.pkl")
-	# with open(Pkl_Filename, 'rb') as file:  
-	# 	Pickled_KNN_Model = pickle.load(file)
-	# return str(Pickled_KNN_Model)
+	if request.method == "POST":
+		
+		try:
+			body = request.form["post-data"]
+			#flash(body,category="info")
+			if(len(body)>0):
+				post = Post(body=body, author=current_user)
+				db.session.add(post)
+				db.session.commit()
+				flash("New tweet was added",category="success")
+			else:
+				flash("Please enter something",category="info")
+			return redirect(url_for('index'))
+		
+		except:
+			db.session.rollback()
+			flash("Error occured",category="danger")
+	
+	posts = current_user.followed_posts()
+	return render_template('home.html',posts=posts)
 
-	return render_template('home.html')
+
 
 @app.route('/login',methods=['GET', 'POST'])
 def login():
@@ -40,6 +56,8 @@ def login():
 		return redirect(url_for('index'))
 	return render_template('login.html',form=form)
 
+
+
 @app.route('/signup',methods=['GET', 'POST'])
 def signup():
 	if current_user.is_authenticated:
@@ -52,12 +70,48 @@ def signup():
 		db.session.commit()
 		flash('Congratulations, you are now a registered user!',"success")
 		return redirect(url_for('login'))
-	return render_template('signup.html',  form=form)
+	return render_template('signup.html', form=form)
 
 
-@app.route('/profile')
-def profile():
-	return render_template('profile.html')
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = user.posts.order_by(Post.timestamp.desc())
+    
+    return render_template('profile.html', user=user, posts=posts)
+
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username),category="info")
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username),category="info")
+    return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username),category="info")
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}.'.format(username),category="info")
+    return redirect(url_for('user', username=username))
 
 
 @app.route('/logout')
